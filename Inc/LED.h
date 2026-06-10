69141b0b10984e380307b26bd31487b90d0f5072
AndersonPhan <Anderson.Phan001@umb.edu>
2026-06-10 02:11:22 -0400

    Add files via upload

---
+++ Inc/LED.h
@@ -0,0 +1,12 @@
+#ifndef __STM32L476G_DISCOVERY_LED_H
+#define __STM32L476G_DISCOVERY_LED_H
+
+#include "stm32l476xx.h"
+
+
+void configure_LED_pin(void);
+void turn_on_LED(void);
+void turn_off_LED(void);
+void toggle_LED(void);
+
+#endif /* __STM32L476G_DISCOVERY_LED_H */
